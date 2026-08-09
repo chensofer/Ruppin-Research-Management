56669308ec571251d4491e0828712d8b0a757956
shachar anafi <shachar3231@gmail.com>
2026-08-09 20:25:05 +0300

    סופי

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-const BASE = process.env.NODE_ENV === 'production' ? '/bgroup11/test1/' : '/';
+const BASE = '/';
 
 export default defineConfig({
   base: BASE,
